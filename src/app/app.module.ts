077f579e593a1b919f14ceaacc9f42b1b105af92
Barath Chinmala <msbarath7@gmail.com>
2020-07-16 11:20:04 +0530

    api connected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskViewComponent
+    TaskViewComponent,
+    NewListComponent
   ],
   imports: [
     BrowserModule,
